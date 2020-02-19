e4a31d53b059d528e67526a099d8e6571e58a6b7
stevecat <sev@provgn.com>
2020-02-19 08:48:06 -0500

    Depends check

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,25 +1,20 @@
 packages:=boost openssl libevent
+darwin_packages:=zeromq
+linux_packages:=zeromq
+native_packages := native_ccache native_comparisontool
 
-protobuf_native_packages = native_protobuf
-protobuf_packages = protobuf
+qt_native_packages = native_protobuf
+qt_packages = qrencode protobuf
 
-qt_packages = qrencode zlib
-
-qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig
-
-rapidcheck_packages = rapidcheck
-
+qt_linux_packages= qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
-wallet_packages=bdb
 
-zmq_packages=zeromq
+wallet_packages=bdb
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_biplist native_ds_store native_mac_alias
-
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages=native_cctools native_cdrkit native_libdmg-hfsplus
 endif
