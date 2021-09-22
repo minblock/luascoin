50cc60f5580c8d171a3f15cec30410d014630d36
stevecat <38344452+minblock@users.noreply.github.com>
2021-09-21 22:52:36 -0400

    Update expat.mk

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,11 +1,11 @@
 package=expat
-$(package)_version=2.4.1
-$(package)_download_path=http://sourceforge.net/projects/expat/files/expat/$($(package)_version)
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=a00ae8a6b96b63a3910ddc1100b1a7ef50dc26dceb65ced18ded31ab392f132b
+$(package)_version=2.2.6
+$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_2_2_6/
+$(package)_file_name=$(package)-$($(package)_version).tar.bz2
+$(package)_sha256_hash=17b43c2716d521369f82fc2dc70f359860e90fa440bea65b3b85f0b246ea81f2
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-static
+$(package)_config_opts=--disable-static --without-docbook
 endef
 
 define $(package)_config_cmds
