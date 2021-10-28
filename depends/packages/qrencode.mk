de39485d8967d66b4d236cd16e8834d879bc559a
stevecat <38344452+minblock@users.noreply.github.com>
2021-10-28 15:07:47 -0400

    Update qrencode.mk

--- depends/packages/qrencode.mk
+++ depends/packages/qrencode.mk
@@ -1,8 +1,8 @@
 package=qrencode
-$(package)_version=3.4.4
-$(package)_download_path=https://fukuchi.org/works/qrencode/
+$(package)_version=4.1.1
+$(package)_download_path=https://provgn.com
 $(package)_file_name=qrencode-$(qrencode_version).tar.bz2
-$(package)_sha256_hash=03d5d3688a2024c5c1d1f61fd5b152fe7c3da56ae84694119d32be74402849ae
+$(package)_sha256_hash=e455d9732f8041cf5b9c388e345a641fd15707860f928e94507b1961256a6923
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared -without-tools --disable-sdltest
