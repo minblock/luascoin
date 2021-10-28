cc6ab0d358a10891119e6a24cb3ec3f69f6dbb61
stevecat <38344452+minblock@users.noreply.github.com>
2021-10-28 14:58:16 -0400

    xyz file

--- depends/packages/qrencode.mk
+++ depends/packages/qrencode.mk
@@ -2,7 +2,7 @@ package=qrencode
 $(package)_version=3.4.4
 $(package)_download_path=https://fukuchi.org/works/qrencode/
 $(package)_file_name=qrencode-$(qrencode_version).tar.bz2
-$(package)_sha256_hash=efe5188b1ddbcbf98763b819b146be6a90481aac30cfc8d858ab78a19cde1fa5
+$(package)_sha256_hash=03d5d3688a2024c5c1d1f61fd5b152fe7c3da56ae84694119d32be74402849ae
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared -without-tools --disable-sdltest
